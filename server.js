434952a925bf2d058e0c44a148006706e265fe1a
unknown <nowpertimmanuel@gmail.com>
2021-08-17 15:53:46 +0530

    intro

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const ejs = require("ejs");
+const https = require("https");
+const path = require("path");
+
+const app = express();
+const server = https.createServer(app);
+
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  //res.send("hello");
+  res.render("login", {});
+});
+
+app.listen(4000, () => {
+  console.log("server running");
+});
