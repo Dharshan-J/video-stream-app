770607ac063f2ccba59dd3ea52afd767c93485ba
nowie03 <54496037+nowie03@users.noreply.github.com>
2021-08-26 14:56:52 +0530

    wishlist done by Nowie

--- public/js/wishlist.js
+++ public/js/wishlist.js
@@ -1,29 +1,27 @@
 const wishlistBtnLove = document.querySelector(".wishlist-btn-love");
 const wishlistBtnLike = document.querySelector(".wishlist-btn-like");
-const movieImgDiv = document.querySelector("#movie_image");
+const movieImg= document.querySelector("#movie_image");
 const movieTitleDiv= document.querySelector("#movie_title");
 const movieAboutDiv=document.querySelector("#movie_about");
-const undoBtn=document.querySelector("#undo_btn");
-undoBtn.classList.add("undo1");
-document.querySelector(".movie-about").classList.remove("disable");
+const undoDiv=document.querySelector(".undo-div");
+
 
 
 
 wishlistBtnLike.addEventListener("click", () => {
     wishlistBtnLike.classList.toggle("d-none");
     wishlistBtnLove.classList.toggle("d-none");
-    undoBtn.classList.remove('undo1');
-    undoBtn.classList.add("undo-btn");
-    movieAboutDiv.classList.remove("hover");
-    movieImgDiv.classList.toggle("disable");
-    movieAboutDiv.classList.toggle("disable");
+    movieImg.classList.toggle("d-none");
+    movieAboutDiv.classList.toggle("d-none");
+    undoDiv.classList.toggle("d-none");
+
 })
-undoBtn.addEventListener("click", () => {
+undoDiv.addEventListener("click", () => {
 
     wishlistBtnLove.classList.toggle("d-none");
-    wishlistBtnLike.classList.toggle("d-none");  
-    movieAboutDiv.classList.add("hover"); 
-    undoBtn.classList.add('undo1');
-    movieImgDiv.classList.toggle("disable");
-    movieAboutDiv.classList.toggle("disable");
+    wishlistBtnLike.classList.toggle("d-none");
+    movieImg.classList.toggle("d-none");
+    movieAboutDiv.classList.toggle("d-none");
+    undoDiv.classList.toggle("d-none");
+
 })
