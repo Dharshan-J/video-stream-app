d8e7ffc99a7da5f58f57f9f97918071f384c8820
Dharshan-J <19euit041@skcet.ac.in>
2021-08-31 20:42:36 +0530

    conflicts removed

--- public/js/videoPlayer.js
+++ public/js/videoPlayer.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // const player = videojs("my-video", {
 //   controls: true,
 //   autoplay: false,
@@ -20,3 +21,31 @@ $(document).ready(function() {
     ply.play();
   });
 });
+=======
+const player = videojs("my-video", {
+  controls: true,
+  autoplay: false,
+  preload: "auto",
+  preferFullWindow: true,
+  html5: {
+    vhs: {
+      withCredentials: true
+    }
+  }
+});
+
+player.hotkeys({
+  volumeStep: 0.1,
+    seekStep: 30,
+    enableModifiersForNumbers: false
+});
+
+player.src({
+      src: "http://sample.vodobox.com/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8",
+  type: "application/x-mpegURL",
+      withCredentials:false
+   
+})
+
+
+>>>>>>> b9c6118ea901478004ed258928c4f181dfa420cd
