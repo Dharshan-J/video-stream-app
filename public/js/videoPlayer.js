b9c6118ea901478004ed258928c4f181dfa420cd
nowie03 <54496037+nowie03@users.noreply.github.com>
2021-08-31 20:35:39 +0530

    video player done

--- public/js/videoPlayer.js
+++ public/js/videoPlayer.js
@@ -2,12 +2,25 @@ const player = videojs("my-video", {
   controls: true,
   autoplay: false,
   preload: "auto",
-  fluid: true,
   preferFullWindow: true,
-  sources: [
-    {
-      src: "../public/videos/big_buck_bunny_720p_1mb.mp4",
-      type: "video/mp4",
-    },
-  ],
+  html5: {
+    vhs: {
+      withCredentials: true
+    }
+  }
 });
+
+player.hotkeys({
+  volumeStep: 0.1,
+    seekStep: 30,
+    enableModifiersForNumbers: false
+});
+
+player.src({
+      src: "http://sample.vodobox.com/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8",
+  type: "application/x-mpegURL",
+      withCredentials:false
+   
+})
+
+
