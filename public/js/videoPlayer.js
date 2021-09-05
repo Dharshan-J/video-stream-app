8dd28ea32be9b38ce7611ed31bfb66329fb376cb
nowie03 <54496037+nowie03@users.noreply.github.com>
2021-09-05 12:19:01 +0530

    dummy commit

--- public/js/videoPlayer.js
+++ public/js/videoPlayer.js
@@ -1,26 +1,24 @@
 const player = videojs("my-video", {
   controls: true,
-  autoplay: false,
+  autoplay: false ,
   preload: "auto",
   preferFullWindow: true,
   html5: {
     vhs: {
-      withCredentials: true
-    }
-  }
+      withCredentials: true,
+    },
+  },
 });
 
 player.hotkeys({
   volumeStep: 0.1,
-    seekStep: 30,
-    enableModifiersForNumbers: false
+  seekStep: 30,
+  enableModifiersForNumbers: false,
+  enableVolumeScroll: false,
 });
 
 player.src({
-      src: "http://sample.vodobox.com/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8",
+  src: "http://sample.vodobox.com/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8",
   type: "application/x-mpegURL",
-      withCredentials:false
-   
-})
-
-
+  withCredentials: false,
+});
